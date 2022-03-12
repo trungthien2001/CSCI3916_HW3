ffdd01f7dccf27253fd2554659e67dd813937c30
trungthien2001 <82750730+trungthien2001@users.noreply.github.com>
2022-03-12 01:47:55 -0700

    Add files via upload

---
+++ Movies.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+mongoose.Promise = global.Promise;
+
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
+
+//movies Schema
+var MoviesSchema = new Schema({
+    title: { type: String, required: true, index: { unique: true }},
+    releaseYear: { type: String, required: true},
+    genre: { type: String, required: true},
+    actors: {type: Array, required: true}
+});
+module.exports = mongoose.model('Movies', MoviesSchema);
