6fa312445324b6de71ca7ed1baec2fa81d3cabcc
trungthien2001 <82750730+trungthien2001@users.noreply.github.com>
2022-03-13 20:29:13 -0600

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,10 +1,10 @@
+let envPath = __dirname + "/../.env"
+require('dotenv').config({path:envPath});
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
 
-mongoose.Promise = global.Promise;
+mongoose.Promise= global.Promise;
 
-//mongoose.connect(process.env.DB, { useNewUrlParser: true });
 try {
     mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
         console.log("connected"));
@@ -13,11 +13,10 @@ try {
 }
 mongoose.set('useCreateIndex', true);
 
-//movies Schema
-var MoviesSchema = new Schema({
-    title: { type: String, required: true, index: { unique: true }},
-    releaseYear: { type: String, required: true},
-    genre: { type: String, required: true},
-    actors: {type: Array, required: true}
+var MovieSchema = new Schema({
+    title:{type:String,required:true,index:{unique:true}},
+    yearReleased:{type:Date, required:true},
+    genre:{type:String,required:true,enum:['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western'] },
+    actors: { type: [{actorName: String, characterName: String}], required: true }
 });
-module.exports = mongoose.model('Movies', MoviesSchema);
+module.exports = mongoose.model('Movie',MovieSchema);
